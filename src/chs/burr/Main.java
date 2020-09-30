98dd7ea7c57e6559bebcfc0c8712325dedd3daaa
Tim <tim.r.burr@gmail.com>
2020-09-30 14:19:43 -0600

    initial commit

---
+++ src/chs/burr/Main.java
@@ -0,0 +1,12 @@
+package chs.burr;
+
+import chs.burr.data.MySqlAccess;
+
+public class Main 
+{
+    public static void main(String[] args) throws Exception 
+    {
+    	MySqlAccess dao = new MySqlAccess();
+        dao.readDataBase();
+    }
+}
